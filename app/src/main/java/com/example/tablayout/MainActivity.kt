891863a1110a27dcd0a410dc08744e68f5c5c987
sergeyesp <sergey_esp@i.ua>
2023-09-30 19:36:36 +0300

    Lesson2

--- app/src/main/java/com/example/tablayout/MainActivity.kt
+++ app/src/main/java/com/example/tablayout/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.tablayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tablayout.adapter.PagerAdapter
+import com.example.tablayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initial()
+    }
+
+    private fun initial() {
+        binding.viewPager.adapter = PagerAdapter(this)
     }
 }
--- app/src/main/java/com/example/tablayout/adapter/PagerAdapter.kt
+++ app/src/main/java/com/example/tablayout/adapter/PagerAdapter.kt
@@ -1,4 +1,22 @@
 package com.example.tablayout.adapter
 
-class PagerAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.tablayout.screens.FirstFragment
+import com.example.tablayout.screens.SecondFragment
+import com.example.tablayout.screens.ThirdFragment
+
+class PagerAdapter(fragmentActivity: FragmentActivity): FragmentStateAdapter(fragmentActivity) {
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position) {
+            0 -> FirstFragment()
+            1 -> SecondFragment()
+            else -> ThirdFragment()
+        }
+    }
 }
